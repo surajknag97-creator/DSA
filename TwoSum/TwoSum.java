432a975a715db14d9f89491ca39478c6a456dc6b
Suraj <suraj.nag@thoughtworks.com>
2026-01-20 23:26:28 +0530

    solving problem for two sum brutforce approach

---
+++ TwoSum/TwoSum.java
@@ -0,0 +1,22 @@
+package TwoSum;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
