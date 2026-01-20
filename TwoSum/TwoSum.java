553bad445fe7bd1806f0ad7a39d317268096a660
Suraj <suraj.nag@thoughtworks.com>
2026-01-20 23:31:18 +0530

    solving ploblem for two sum optimised solution

--- TwoSum/TwoSum.java
+++ TwoSum/TwoSum.java
@@ -1,13 +1,17 @@
 package TwoSum;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] {numMap.get(complement), i};
             }
+            numMap.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
